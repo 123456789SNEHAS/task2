32defac49ecf790950bcd166b4dddb69a633702e
123456789SNEHAS <snehagowda10s@gmail.com>
2025-03-21 12:37:58 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="login.php" method="POST">
+        <input type="text" name="email_or_username" placeholder="Email or Username" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <button type="submit" name="login">Login</button>
+    </form>
+</body>
+</html>
